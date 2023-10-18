f486e0139385d62b90116a57ce6ccf1d27b12a45
User001 <12345@12345.com>
2023-10-18 11:10:04 +0900

    Dynamic Moving of KPS Pages : 1 to 31, updated requir.txt '

--- Dynamic_Crawling_KiprisPS_Python_Start1.py
+++ Dynamic_Crawling_KiprisPS_Python_Start1.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import requests
 
 import time
 
 driver = webdriver.Chrome()
 
-driver.get('http://kpat.kipris.or.kr/kpat/searchLogina.do?next=MainSearch')
 
-time.sleep(2)
+url = 'http://kpat.kipris.or.kr/kpat/searchLogina.do?next=MainSearch#page1'
+
+driver.get(url)
+time.sleep(1.5)
 
 act =ActionChains(driver)
 
@@ -17,19 +21,45 @@ act.click(search_box).perform()
 
 search_box.send_keys('핸드폰')
 search_box.send_keys(Keys.RETURN)
-time.sleep(3)
+time.sleep(1.5)
+
 
 HJ_ALL_Box = driver.find_element("id", "leftHangjung00")
+
 HJ_Decl_Box = driver.find_element("id", "leftHangjung06")
 HJ_Acpt_Box = driver.find_element("id", "leftHangjung07")
 HJ_OK_Box = driver.find_element("xpath", "//*[@id='leftside']/div/span/a/img")
 
 act.click(HJ_ALL_Box).perform()
-
 act.click(HJ_Decl_Box).perform()
-
 act.click(HJ_Acpt_Box).perform()
 
 act.click(HJ_OK_Box).perform()
 
-time.sleep(7)
+# 웹 페이지 초기 검색조건 정렬.(동적 웹크롤링)
+
+
+for PMRS1 in range(1, 4):
+
+    for SP1 in range(2, 12):
+
+
+        if SP1 != 11:
+
+            NumbeFF = SP1 + 1
+            PButtn_2 = driver.find_element(By.CSS_SELECTOR, "#divBoardPager > a:nth-child({})".format(NumbeFF))
+            act.click(PButtn_2).perform()
+            time.sleep(2)
+
+        else:
+            PButtn_2 = driver.find_element(By.CSS_SELECTOR, "#divBoardPager > a.next")
+            act.click(PButtn_2).perform()
+            time.sleep(2)
+
+
+time.sleep(3)
+
+
+
+
+
