40a7a206b396869f6519f8f87153ffd241ed12f8
User001 <12345@12345.com>
2023-10-19 14:44:11 +0900

    Dynamic Moving of KPS Pages : 1 to 1500, No Labeled data processing.

--- Pandas_Databas_Bake1.py
+++ Pandas_Databas_Bake1.py
@@ -19,14 +19,15 @@ datafra.to_csv('./DataBS_For_Training_1/ArtcsE2.csv', encoding='utf-8-sig')
 datafra.reset_index(inplace=True)
 datafra.columns = ["Patent Summary", "Result"]
 print(datafra)
-datafra.to_csv('./DataBS_For_Training_1/ArtcsE33.csv', encoding='utf-8-sig')
+datafra.to_csv('./DataBS_For_Training_1/ArtcsE33.csv', index = False, encoding='utf-8-sig')
 
 ### 인덱스 리셋 후 컬럼 인덱스 추가
 ### 인덱스를 제거해야할 경우 index = False를 to_csv 메서드에 추가하면 된다.
 
 
 
-temp_list = ['거절', '등록']
-datafra.insert(2, 'Rslt_Lbl', datafra.Result.map(lambda x: temp_list.index(x)))
-datafra.to_csv('./DataBS_For_Training_1/ArtcsE44.csv', encoding='utf-8-sig')
+# ### 데이터 라벨링 코드 ### 기호에 따라 사용 ### 010101
+# temp_list = ['거절', '등록']
+# datafra.insert(2, 'Rslt_Lbl', datafra.Result.map(lambda x: temp_list.index(x)))
+# datafra.to_csv('./DataBS_For_Training_1/ArtcsE44.csv', encoding='utf-8-sig')
 
